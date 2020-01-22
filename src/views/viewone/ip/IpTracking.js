d9f04571887f92fc33bea46676ff110a99648d6a
Lewis Luckhurst <lewisluckhurst@icloud.com>
2020-01-22 19:59:56 +0000

    Removing continent name as it is no longer provided by the api

--- src/views/viewone/ip/IpTracking.js
+++ src/views/viewone/ip/IpTracking.js
@@ -10,7 +10,6 @@ class IpTracking extends Component {
                 <div className="table">
                     <div className="secondColumn">
                         <p>Ip Address:</p>
-                        <p>Continent Name:</p>
                         <p>Country Name:</p>
                         <p>City:</p>
                         <p>Post code:</p>
@@ -18,7 +17,6 @@ class IpTracking extends Component {
                     </div>
                     <div className="thirdColumn">
                         <p>{this.props.result["ip"]}</p>
-                        <p>{this.props.result["continentName"]}</p>
                         <p>{this.props.result["countryName"]}</p>
                         <p>{this.props.result["city"]}</p>
                         <p>{this.props.result["postCode"]}</p>
